a626663b3c6f0ebbde7612af0c219d47da7ea6ec
zmateusz <mat309@o2.pl>
2015-09-23 19:32:30 +0200

    validate contact params

--- ruby/02_sinatra_camp_api/app.rb
+++ ruby/02_sinatra_camp_api/app.rb
@@ -13,6 +13,21 @@ helpers do
     data = File.read("json/page#{page}.json")
     data = JSON.parse(data, symbolize_names: false).to_json
   end
+
+  def validate(name, email, message) 
+    errors = {}
+    errors[:name] = ["nie może być puste"] if name.nil? || name.empty?
+    email_errors = []
+    email_errors << "nie może być puste" if email.nil? || email.empty?
+    email_errors << "jest nieprawidłowe" if (email =~ /@/).nil?
+    errors[:email] = email_errors if !email_errors.empty?
+    errors[:message] = ["nie może być puste"] if message.nil? || message.empty?
+    if errors.empty?
+      {name: name, email: email, message: message}
+    else
+      {errors: errors}
+    end
+  end
 end
 
 error 500 do
@@ -31,8 +46,12 @@ end
 
 # CREATE
 # /camp/api/contact?name=John&email=j@j.com&message=Hello
-post '/camp/api/contact' do 
-  data = {name: params[:name],
-          email: params[:email],
-          message: params[:message]}.to_json
+post '/camp/api/contact' do
+  data = validate(params[:name], params[:email], params[:message])
+  if data.has_key?(:errors)
+    status 422
+    data.to_json
+  else
+    data.to_json
+  end
 end
